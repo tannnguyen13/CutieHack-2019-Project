3fffd0d2fd5bbcecd86e78fcb2a1ef793d6012c2
nickthechang <57569144+nickthechang@users.noreply.github.com>
2019-11-09 15:29:38 -0800

    Add files via upload

---
+++ Game.h
@@ -0,0 +1,19 @@
+#ifndef CUTIEHACK_2019_PROJECT_ITEM_H
+#define CUTIEHACK_2019_PROJECT_ITEM_H
+#include <string>
+using namespace std;
+
+class Game{
+    public:
+        Item(int, int);
+        const getStrength();
+        const getDefense();
+        setHealth();
+    private:
+        int strength;
+        int defense;
+};
+
+
+
+#endif //CUTIEHACK_2019_PROJECT_ITEM_H
