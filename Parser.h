db75aedb19b5c978b07c1dd0d5409a84e0a3b3e3
tannnguyen13 <55606359+tannnguyen13@users.noreply.github.com>
2019-11-09 13:52:18 -0800

    Add files via upload

--- Parser.h
+++ Parser.h
@@ -5,4 +5,19 @@
 #ifndef CUTIEHACK_2019_PROJECT_PARSER_H
 #define CUTIEHACK_2019_PROJECT_PARSER_H
 
+#include "CommandWords.h"
+#include "Command.h"
+#include <sstream>
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Parser {
+private:
+    CommandWords commands;
+public:
+    Parser();
+    Command getCommand();
+    void showCommands();
+};
 #endif //CUTIEHACK_2019_PROJECT_PARSER_H
