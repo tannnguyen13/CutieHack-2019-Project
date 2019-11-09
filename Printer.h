5ea5cbab261eeaa3b4fb67bfad743f4a8702e4a0
tannnguyen13 <55606359+tannnguyen13@users.noreply.github.com>
2019-11-09 14:17:16 -0800

    Add files via upload

--- Printer.h
+++ Printer.h
@@ -4,5 +4,19 @@
 
 #ifndef CUTIEHACK_2019_PROJECT_PRINTER_H
 #define CUTIEHACK_2019_PROJECT_PRINTER_H
-
+#include <iostream>
+#include <string>
+using namespace std;
+class Printer {
+public:
+    void print() {
+        string thing;
+        cin >> thing;
+        int delay = 200;
+        for(int i = 0; i < thing.size(); i++) {
+            cout << thing[i];
+            Sleep(delay)
+        }
+    }
+};
 #endif //CUTIEHACK_2019_PROJECT_PRINTER_H
