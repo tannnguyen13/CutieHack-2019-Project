cb44182d3a921ebd0d989fff4b3a4608ea33ec93
Vy Vo <56359868+vyvanvo@users.noreply.github.com>
2019-11-09 15:37:27 -0800

    Update Printer.h

--- Printer.h
+++ Printer.h
@@ -1,24 +1,15 @@
-//
-// Created by Tann Nguyen on 11/9/19.
-//
-
-#ifndef CUTIEHACK_2019_PROJECT_PRINTER_H
-#define CUTIEHACK_2019_PROJECT_PRINTER_H
+#ifndef PRINT_H
+#define PRINT_H
 #include <iostream>
 #include <string>
 #include <thread>
 #include <chrono>
+
 using namespace std;
+
 class Printer {
 public:
-    void print() {
-        string thing;
-        cin >> thing;
-        int delay = 200;
-        for(int i = 0; i < thing.size(); i++) {
-            cout << thing[i] << flush;
-            this_thread::sleep_for(chrono::milliseconds(delay));
-        }
-    }
+    void print();
 };
-#endif //CUTIEHACK_2019_PROJECT_PRINTER_H
+
+#endif 
